869749755de486b6d1cfdc41a9e3d5c00fc3a7ab
Lucas Alvares <lucas.alvaress@hotmail.com>
2024-01-19 22:04:15 -0300

    ✨ create login page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Image, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {styles} from './styles';
 
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleLogin() {
+    if (email === '' || password === '') {
+      return;
+    }
+
+    console.log(email, password);
+  }
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('../../assets/logo.png')} />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="E-mail"
+          style={styles.input}
+          placeholderTextColor="#FFF"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          placeholder="Password"
+          style={styles.input}
+          placeholderTextColor="#FFF"
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Sign in</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,26 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import AppRoutes from './app.routes';
 import AuthRoutes from './auth.routes';
+import {View, ActivityIndicator} from 'react-native';
 
 function Routes() {
   const isAuthenticaded = false;
   const loading = false;
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#1D1D2E',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <ActivityIndicator size={60} color="#FFF" />
+      </View>
+    );
+  }
 
   return isAuthenticaded ? <AppRoutes /> : <AuthRoutes />;
 }
